7c406d763a26b0584a0e6659c384fad0738753c4
Tony <syntony666@outlook.com>
2019-06-01 01:31:42 +0800

    finaish no 18

--- web_crawler/ptt.py
+++ web_crawler/ptt.py
@@ -2,10 +2,20 @@ from bs4 import BeautifulSoup
 import requests
 
 
+class Push():
+    def __init__(self, url):
+        res = BeautifulSoup(requests.get(url).text, 'html.parser')
+        self.p = [x.string
+                  for x in res.html.find_all(['div'], class_=['nrec'])]
+        self.push = [x if (x != None) else '0' for x in self.p]
+
+    def str(self):
+        return self.push
+
+
 class Title():
     def __init__(self, url):
         res = BeautifulSoup(requests.get(url).text, 'html.parser')
-
         self.a1 = [x.get_text().replace('\n', '').replace('\t', '')
                    for x in res.html.find_all(['div'], class_=['title'])]
 
@@ -31,3 +41,11 @@ class Date():
 
     def str(self):
         return self.date
+
+
+def previous(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    u = soup.select("div.btn-group.btn-group-paging a")
+    url = "https://www.ptt.cc" + u[1]["href"]
+    return url
