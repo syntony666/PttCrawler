97170dfdfe3a5deda5e0fa524b97c3fcbb7ea9f5
Tony <syntony666@outlook.com>
2019-06-07 07:30:11 +0800

    all finished

--- ptt_crawler/ptt_main.py
+++ ptt_crawler/ptt_main.py
@@ -1,6 +1,21 @@
 from src.crawler import *
 
 
+def ptt(board, page, is18):
+    url = 'https://www.ptt.cc/bbs/' + board + '/index.html'
+    try:
+        for i in range(page):
+            if is18 == True:
+                res = ask18(board, url)
+            else:
+                res = BeautifulSoup(requests.get(url).text, 'html.parser')
+            print('-'*100 + '\n\n'+'本頁為: ' + url + ' (第 ' + str(i + 1) + ' 頁)' + '\n')
+            url = dataOutput(res)
+        print('-'*100 + '\n')
+    except IndexError:
+        print('看板名稱不正確\n')
+
+
 def main():
     try:
         board = input("請輸入看板名稱: ")
--- ptt_crawler/src/crawler.py
+++ ptt_crawler/src/crawler.py
@@ -11,7 +11,7 @@ def previous(res):
     return url
 
 
-def crawler(res):
+def dataOutput(res):
     m = Meta(res)
     for a, b, c, d, e in zip(m.push(), m.title(), m.date(), m.author(), m.link()):
         pretty_print(a, b, c, d)
@@ -28,18 +28,3 @@ def ask18(board, url):
     r1 = r.post('https://www.ptt.cc/ask/over18?from=%2Fbbs%2F' +
                 board + '%2Findex.html', payload)
     return BeautifulSoup(r.get(url).text, 'html.parser')
-
-
-def ptt(board, page, is18):
-    url = 'https://www.ptt.cc/bbs/' + board + '/index.html'
-    try:
-        for i in range(page):
-            if is18 == True:
-                res = ask18(board, url)
-            else:
-                res = BeautifulSoup(requests.get(url).text, 'html.parser')
-            print('-'*100 + '\n\n'+'本頁為: ' + url + ' (第 ' + str(i + 1) + ' 頁)' + '\n')
-            url = crawler(res)
-        print('-'*100 + '\n')
-    except IndexError:
-        print('看板名稱不正確\n')
